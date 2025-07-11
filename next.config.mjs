e9d8704c263dfd5f1ea2a4e5fca3052069d0bdc2
PhianaDv <phiana.devilliers@gmail.com>
2025-07-11 10:28:41 +0200

    u

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
     output: 'export', // static export
   basePath: '/cloud-nine-coaches',
   assetPrefix: '/cloud-nine-coaches/',
+  images: { unoptimized: true }
+  
 };
 
 export default nextConfig;
